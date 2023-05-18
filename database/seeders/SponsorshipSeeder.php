936c72dbce20f215b6c6dff9df87a753216a00f3
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-05-18 18:24:41 +0200

    FEAT Sponsorship seeder completed

--- database/seeders/SponsorshipSeeder.php
+++ database/seeders/SponsorshipSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Sponsorship;
 
 class SponsorshipSeeder extends Seeder
 {
@@ -14,6 +15,34 @@ class SponsorshipSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $sponsorships = [
+            [
+                'name' => 'Spotlight Stay',
+                'duration' => 24,
+                'price' => 2.99
+            ],
+            [
+                'name' => 'Featured Getaway',
+                'duration' => 72,
+                'price' => 5.99
+            ],
+            [
+                'name' => 'Exclusive retreat',
+                'duration' => 144,
+                'price' => 9.99
+            ],
+        ];
+
+        foreach ($sponsorships as $sponsorship) {
+            
+            $new_sponsorship = new Sponsorship();
+
+            $new_sponsorship->name = $sponsorship['name'];
+            $new_sponsorship->duration = $sponsorship['duration'];
+            $new_sponsorship->price = $sponsorship['price'];
+
+            $new_sponsorship->save();
+        }
     }
 }
