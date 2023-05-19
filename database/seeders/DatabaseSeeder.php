3ac7bbb9591ced39b26439030c0cf0bcd42d543e
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-19 16:00:05 +0200

    FEAT db seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
             SponsorshipSeeder::class,
             ServiceSeeder::class,
             ApartmentSeeder::class,
+            ApartmentSponsorshipSeeder::class,
             MessageSeeder::class,
             ViewSeeder::class,
             ImageSeeder::class,
