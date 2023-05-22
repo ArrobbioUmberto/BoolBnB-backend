67beff09691a5ab9550fbfef88fcd139d01c2ff7
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-22 15:15:03 +0200

    FEAT print apartments in table with foreach

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -23,17 +23,23 @@
             </thead>
 
             <tbody>
-                <tr>
-                    <td>Casa al mare</td>
-                    <td>San Teodoro</td>
-                    <td>via ho voglia di mare 10</td>
-                    <td>120&euro;</td>
-                    <td>80 &#13217;</td>
-                    <td>6</td>
-                    <td>2</td>
-                    <td>1</td>
-                    <td>privata</td>
-                </tr>
+
+                @foreach ($apartments as $apartment)
+
+                    <tr>
+                        <td>{{ $apartment->title }}</td>
+                        <td>{{ $apartment->city }}</td>
+                        <td>{{ $apartment->address }}</td>
+                        <td>{{ $apartment->price }}&euro;</td>
+                        <td>{{ $apartment->sqm }} &#13217;</td>
+                        <td>{{ $apartment->rooms }}</td>
+                        <td>{{ $apartment->beds }}</td>
+                        <td>{{ $apartment->bathrooms }}</td>
+                        <td>{{ $apartment->visibility == 1 ? "pubblico" : "privato" }}</td>
+                    </tr>
+
+                @endforeach
+
             </tbody>
         </table>
     </div>
