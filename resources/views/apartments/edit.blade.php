81b9fd2d40b46d3d510366204aed8f19abf0a751
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-22 19:07:59 +0200

    FEAT set old value

--- resources/views/apartments/edit.blade.php
+++ resources/views/apartments/edit.blade.php
@@ -10,7 +10,8 @@
         <div class="mb-3">
             <label for="cover_image" class="form-label">Immagine di copertina</label>
             <input type="file" name="cover_image" class="form-control @error('cover_image') is-invalid @enderror"
-                value="{{ old('cover_image') }}" id="cover_image" aria-describedby="cover_image">
+                value="{{ old('cover_image', $apartment->cover_image) }}" id="cover_image"
+                aria-describedby="cover_image">
             @error('cover_image')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -20,7 +21,7 @@
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
             <input type="text" name="title" class="form-control @error('title') is-invalid @enderror"
-                value="{{ old('title') }}" id="title" aria-describedby="title">
+                value="{{ old('title', $apartment->title) }}" id="title" aria-describedby="title">
             @error('title')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -30,7 +31,7 @@
         <div class="mb-3">
             <label for="rooms" class="form-label">Stanze</label>
             <input type="number" name="rooms" class="form-control @error('rooms') is-invalid @enderror"
-                value="{{ old('rooms') }}" id="rooms" aria-describedby="rooms">
+                value="{{ old('rooms', $apartment->rooms) }}" id="rooms" aria-describedby="rooms">
             @error('rooms')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -40,7 +41,7 @@
         <div class="mb-3">
             <label for="beds" class="form-label">Posti letto</label>
             <input type="number" name="beds" class="form-control @error('beds') is-invalid @enderror"
-                value="{{ old('beds') }}" id="beds" aria-describedby="beds">
+                value="{{ old('beds', $apartment->beds) }}" id="beds" aria-describedby="beds">
             @error('beds')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -50,7 +51,7 @@
         <div class="mb-3">
             <label for="bathrooms" class="form-label">Bagni</label>
             <input type="number" name="bathrooms" class="form-control @error('bathrooms') is-invalid @enderror"
-                value="{{ old('bathrooms') }}" id="bathrooms" aria-describedby="bathrooms">
+                value="{{ old('bathrooms', $apartment->bathrooms) }}" id="bathrooms" aria-describedby="bathrooms">
             @error('bathrooms')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -60,7 +61,7 @@
         <div class="mb-3">
             <label for="sqm" class="form-label">Mq</label>
             <input type="number" name="sqm" class="form-control @error('sqm') is-invalid @enderror"
-                value="{{ old('sqm') }}" id="sqm" aria-describedby="sqm">
+                value="{{ old('sqm', $apartment->sqm) }}" id="sqm" aria-describedby="sqm">
             @error('sqm')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -70,7 +71,7 @@
         <div class="mb-3">
             <label for="address" class="form-label">Indirizzo</label>
             <input type="text" name="address" class="form-control @error('address') is-invalid @enderror"
-                value="{{ old('address') }}" id="address" aria-describedby="address">
+                value="{{ old('address', $apartment->address) }}" id="address" aria-describedby="address">
             @error('address')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -80,7 +81,7 @@
         <div class=" mb-3">
             <label for="city" class="form-label">Città</label>
             <input type="text" name="city" class="form-control @error('city') is-invalid @enderror"
-                value="{{ old('city') }}" id="city" aria-describedby="city">
+                value="{{ old('city', $apartment->city) }}" id="city" aria-describedby="city">
             @error('city')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -92,7 +93,8 @@
             <div class="input-group ">
                 <span class="input-group-text">€</span>
                 <input type="text" name="price" class="form-control @error('price') is-invalid @enderror"
-                    value="{{ old('price') }}" id="price" aria-label="Amount (to the nearest dollar)">
+                    value="{{ old('price', $apartment->price) }}" id="price"
+                    aria-label="Amount (to the nearest dollar)">
             </div>
             @error('price')
                 <div class="invalid-feedback">
@@ -102,8 +104,10 @@
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea name="description" class="form-control @error('description') is-invalid @enderror"
-                value="{{ old('description') }}" id="description" aria-label="With textarea" cols="30" rows="10"></textarea>
+            <textarea name="description" class="form-control @error('description') is-invalid @enderror" value=""
+                id="description" aria-label="With textarea" cols="30" rows="10">
+                {{ old('description', $apartment->description) }}
+            </textarea>
             @error('description')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -112,10 +116,10 @@
         </div>
         <div class="mb-3">
             <label class="form-label" for="services"> Servizi</label>
-            @foreach ($services as $key => $service)
+            @foreach ($services as $service)
                 <div class="form-check">
-                    <input type="checkbox" name="services[]" @checked(in_array($service->ids, old('services', $apartment->getServiceIds()))) class="form-check-input "
-                        value="{{ $service->ids }}" id="flexCheckDefault">
+                    <input type="checkbox" name="services[]" @checked(in_array($service->id, old('services', $apartment->getServiceIds()))) class="form-check-input "
+                        value="{{ $service->id }}" id="flexCheckDefault">
                     <label for="flexCheckDefault" class="form-check-label">{{ $service->name }}</label>
                 </div>
             @endforeach
@@ -123,10 +127,10 @@
         <div class="mb-3">
             <label for="visibility" class="form-label">Visibilità</label>
             <select class="form-select @error('visibility') is-invalid @enderror" aria-label="Default select example"
-                id="visibility" name="visibility">
-                <option value="" selected>Vuoi rendere visibile il tuo appartamento ?</option>
-                <option value="1">SI</option>
-                <option value="0">NO</option>
+                id="visibility" name="visibility" value="{{ old('visibility', $apartment->visibility) }}">
+                <option selected>Vuoi rendere visibile il tuo appartamento ?</option>
+                <option @selected(old('visibility', $apartment->visibility) == 1)value="1">SI</option>
+                <option @selected(old('visibility', $apartment->visibility) == 0)value="0">NO</option>
 
             </select>
             @error('visibility')
