841b50efe4733cfbbf4d2b589e6c8f8e074e4798
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-05-22 15:05:41 +0200

    FEAT table in apartaments index added

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -2,27 +2,37 @@
 
 @section('content')
 
-    <div class="container">
-        <table>
+    <div class="container pt-3">
+        <h1>ELenco degli appartamenti</h1>
+    </div>
+
+    <div class="container py-5">
+        <table class="table table-striped table-inverse table-responsive">
             <thead>
                 <tr>
                     <th>Titolo</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <th>Città</th>
+                    <th>Indirizzo</th>
+                    <th>Prezzo</th>
+                    <th>Metri quadrati</th>
+                    <th>N. stanze</th>
+                    <th>N. letti</th>
+                    <th>N. bagni</th>
+                    <th>Visibilità</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
+                    <td>Casa al mare</td>
+                    <td>San Teodoro</td>
+                    <td>via ho voglia di mare 10</td>
+                    <td>120&euro;</td>
+                    <td>80 &#13217;</td>
+                    <td>6</td>
+                    <td>2</td>
+                    <td>1</td>
+                    <td>privata</td>
                 </tr>
             </tbody>
         </table>
