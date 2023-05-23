614ece1f5bdbeff0cee66c59a1b3806e91a49066
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-05-23 18:01:33 +0200

    FEAT validation rules

--- app/Http/Requests/StoreApartmentRequest.php
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -24,17 +24,18 @@ class StoreApartmentRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'=> "required|string|max:255|unique:apartments,title",
-            'rooms'=> "required|number|min:1|max:40",
-            'beds'=> "required|number|min:1|max:30",
-            'bathrooms'=> "required|number|min:1|max:20",
-            'sqm'=> "required|number|min:5|max:1000",
-            'address'=> "required|string|min:7|max:255",
-            'city'=> "required|string|min:2|max:100",
-            'visibility'=> "required|boolean",
-            'price'=> "required|decimal:2|min:20|max:9999",
-            'description'=> "required|string|min:8|max:2000",
-            'cover_image'=> "required|image|max:3072",
+            'title' => "required|string|min:8|max:255|unique:apartments,title",
+            'rooms' => "required|numeric|min:1|max:40",
+            'beds' => "required|numeric|min:1|max:30",
+            'bathrooms' => "required|numeric|min:1|max:20",
+            'sqm' => "required|numeric|min:5|max:1000",
+            'address' => "required|string|min:7|max:255",
+            'city' => "required|string|min:2|max:100",
+            'visibility' => "required|boolean",
+            'price' => "required|numeric|min:20|max:9999",
+            'description' => "required|string|min:8|max:2000",
+            'cover_image' => "required|image|max:3072",
+            'services' => "exists:services,id"
         ];
     }
 }
