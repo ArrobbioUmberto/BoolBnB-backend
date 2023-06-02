fe5aa50effca5a354a88b1aa5aa1fe5d778603ab
Brody14 <stefania.spinas@gmail.com>
2023-06-02 14:54:39 +0200

    EDIT back buttons and dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,28 +7,31 @@
     </h2>
     <div class="row justify-content-center">
         <div class="col">
-            <div class="card">
+            <div class="card bool-card-dark">
                 <div class="card-header card-hd">{{ __('User Dashboard') }}</div>
 
-                <div class="row p-5">
-                    <div class="col-4 fs-1 text-center">
+                <div class="row p-5 justify-content-center icon">
+                    <div class="col-3 fs-1 text-center">
                         <a href="{{ url('/apartments') }}">
-                            <div class="card bool-card p-3">
-                                <i class="fa-solid fa-house"></i>
+                            <div class="card bool-card-dark p-3">
+                                <i class="fa-solid fa-house mb-3"></i>
+                                <h5>I tuoi appartamenti</h5>   
                             </div>
                         </a>
                     </div>
-                    <div class="col-4 fs-1 text-center">
+                    <div class="col-3 fs-1 text-center">
                         <a href="{{ url('/messages') }}">
-                            <div class="card bool-card p-3">
-                                <i class="fa-solid fa-envelope"></i>
+                            <div class="card bool-card-dark p-3">
+                                <i class="fa-solid fa-envelope mb-3 message"></i>
+                                <h5>I tuoi messaggi</h5>  
                             </div>
                         </a>
                     </div>
-                    <div class="col-4 fs-1 text-center">
-                        <a href="{{ url('profile') }}">
-                            <div class="card bool-card p-3">
-                                <i class="fa-solid fa-user"></i>
+                    <div class="col-3 fs-1 text-center">
+                        <a href="{{ url('/profile') }}">
+                            <div class="card bool-card-dark p-3">
+                                <i class="fa-solid fa-user mb-3 user"></i>
+                                <h5>Il tuo profilo</h5>  
                             </div>
                         </a>
                     </div>
